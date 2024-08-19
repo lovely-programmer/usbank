a0b99e476f3f854f0b9ed3078aeea3946027439b
Olaoluwa Olasunkanmi <85916649+lovely-programmer@users.noreply.github.com>
2024-08-19 21:49:00 +0100

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -53,46 +53,53 @@ function TransactionHistory() {
                 </tr>
               ))}
               <tr>
-                <td> 12/03/2022</td>
+                <td> 07/15/2024</td>
                 <td>Credit</td>
                 <td>800000</td>
                 <td>Management</td>
                 <td>Chemical Purchase</td>
               </tr>
               <tr>
-                <td> 7/02/2022</td>
+                <td> 06/10/2024</td>
                 <td>Debit</td>
                 <td>200000</td>
                 <td>LAMBERTH</td>
                 <td>Supplies</td>
               </tr>
               <tr>
-                <td>06/05/2022</td>
+                <td>05/12/2024</td>
                 <td>Debit</td>
                 <td>150000</td>
                 <td>RAMSAY HEALTH CARE UK</td>
                 <td>Supplies</td>
               </tr>
               <tr>
-                <td>07/10/2021</td>
+                <td>04/17/2024</td>
                 <td>Debit</td>
                 <td>100000</td>
                 <td>QUEENS VICTORIA HOSPITAL</td>
                 <td>Medications</td>
               </tr>
               <tr>
-                <td>06/20/2021</td>
+                <td>03/20/2024</td>
                 <td>Credit</td>
                 <td>56000</td>
                 <td>Antonio Gonzalo</td>
                 <td>Credit</td>
               </tr>
               <tr>
-                <td>06/05/2021</td>
+                <td>01/25/2024</td>
                 <td>Debit</td>
                 <td>200000</td>
                 <td>RAMSAY HEALTH CARE UK</td>
                 <td>Transplant Surgery</td>
+              </tr>
+              <tr>
+                <td> 12/15/2023</td>
+                <td>Credit</td>
+                <td>170000</td>
+                <td>Management</td>
+                <td>Chemical Purchase</td>
               </tr>
             </tbody>
           </table>
